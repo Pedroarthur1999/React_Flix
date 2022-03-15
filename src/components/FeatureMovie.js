0c3316abefdb27dcdf17bcb71f3a411505f5de9d
Pedroarthur1999 <pedroarthur1999@gmail.com>
2022-03-14 23:47:48 -0300

    done header menu

--- src/components/FeatureMovie.js
+++ src/components/FeatureMovie.js
@@ -2,6 +2,14 @@
 import React from "react";
 import "./FeatureMovie.css";
 export default ({ item }) => {
+
+      let genres = [];
+
+      for(let i in item.genres){
+        genres.push(item.genres[i].name);
+      }
+
+   console.log(genres);
   return (
     <section
       className="featured"
@@ -11,12 +19,23 @@ export default ({ item }) => {
     >
       <div className="featured--vertical">
         <div className="featured--horizontal">
-          <div className="featured--information">
+          <div className="logo-netflix">NETFLIX</div>
             <div className="featured--name">{item.original_name}</div>
-            <div className="featured--classification">{`${item.vote_average} Pontos`}</div>
-            <div className="featured--data">{item.first_air_date.slice(0, 4)}</div>
-            <div className="featured--seasons">{`${item.number_of_seasons} temporadas`}</div>
+              <div className="featured--information">
+              <div className="featured--classification">{`${item.vote_average} Pontos`}</div>
+              <div className="featured--data">{item.first_air_date.slice(0, 4)}</div>
+              <div className="featured--seasons">{`${item.number_of_seasons} temporadas`}</div>
+            </div>
+          <div className="featured-description">
+            <h1>{item.overview}</h1>
           </div>
+           <div className="buttons">
+             <a href={`/watch/${item.id}`} className="button_play"> ►Assistir</a> 
+             <a href={`/list/add/${item.id}`} className="button_list"> + Minha Lista</a>
+           </div>
+           <div className="featured--genres"><strong>Gêneros:</strong>{genres.join(', ')}</div>
+
+
         </div>
       </div>
     </section>
